8a7904698a371e168abd29ae197b69caf08f5a48
Lucas Lafèré <laferelucas@gmail.com>
2022-05-24 22:08:43 -0300

    logo add?

--- src/Components/TelaCadastro.js
+++ src/Components/TelaCadastro.js
@@ -2,8 +2,27 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
+import logo from '../Assets/logo.png'
 
 
 export default TelaCadastro() {
 
+
+
+
+    return (
+        <Container>
+            <LogoBox>
+                <img src={logo} alt="logo trackit" />
+            </LogoBox>
+        </Container>
+    )
 }
+
+const Container = styled.div`
+
+`
+
+const LogoBox = styled.div`
+
+`
